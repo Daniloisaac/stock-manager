a5de347b60d0a9f24e6b72d8e8060b73511e6591
danilo isaac <daniloisaac942@gmail.com>
2023-03-13 19:18:12 -0300

    feat: criei migration e model de sales

---
+++ app/backend/src/database/models/sales.ts
@@ -0,0 +1,24 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../config/SequelizeConfig';
+
+export default class Sales extends Model {
+  declare id:number;
+  declare name:string; 
+}
+
+Sales.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  date: {
+    type: DataTypes.STRING,
+  }
+}, {
+  sequelize,
+  tableName: 'sales',
+  timestamps: false,
+})
+
