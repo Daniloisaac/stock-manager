91428919d21582cc40b7ff22e202f5ab288694f6
danilo isaac <daniloisaac942@gmail.com>
2023-03-17 15:43:24 -0300

    feat: adicionei validação no metodo insertNewProduct

--- app/backend/src/services/Products.service.ts
+++ app/backend/src/services/Products.service.ts
@@ -2,6 +2,8 @@ import ProductsModel from '../database/models/Products.model'
 
 export default class ProductsService {
   public async insertNewProduct (name: string) {
+    if (!name) return { messageError: 'name do product is required' }
+
     const newProduct = await ProductsModel.create({ name })
     return newProduct
   }
@@ -13,7 +15,6 @@ export default class ProductsService {
 
   public async getProductById (id: string) {
     const product = await ProductsModel.findByPk(id)
-    console.log(product)
     return product
   }
 }
